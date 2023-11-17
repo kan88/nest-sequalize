2558787bd28d56c88e13e51df2c706a9921c3251
Евгений <msk9199625687@yandex.ru>
2023-11-17 13:55:41 +0300

    feat: nest cache-manager

--- src/0002-photos/photos.module.ts
+++ src/0002-photos/photos.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Photo } from './photos.model';
 import { PhotosService } from './photo.service';
+CacheModule
 
 @Module({
   providers: [PhotosService],
